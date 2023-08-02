526245cde25128e22d099f7b42ae2f54ef1bf403
blessing324 <idowublessingj@gmail.com>
2023-08-02 13:29:59 +0100

    checking out various datatypes

--- Introduction/datatype.js
+++ Introduction/datatype.js
@@ -2,3 +2,9 @@ let firstName = 'Idowu' //this is a string value
 let isSingle = 'false'
 let undefined
 let nullValue = 'null'
+
+
+typeof('firstName')
+typeof('isSingle')
+typeof('undefined')
+typeof('nullValue')
